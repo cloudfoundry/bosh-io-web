e92c042d00bd62af50d2f993c38261dbc8f90e60
dmitriy kalinin <dkalinin@pivotal.io>
2015-07-01 11:50:15 -0700

    map cloudfoundry.org routes

--- push-cf.sh
+++ push-cf.sh
@@ -33,6 +33,8 @@ fi
 echo "Mapping routes to new version"
 cf unmap-route $new cfapps.io -n $new
 cf map-route $new cfapps.io -n $curr
+cf map-route $new cfapps.io -n bosh
+cf map-route $new cloudfoundry.org -n bosh
 cf map-route $new bosh.io -n www
 cf map-route $new bosh.io
 
@@ -42,6 +44,8 @@ cf rename $curr $old
 echo "Unmapping routes from old version"
 cf map-route $old cfapps.io -n $old
 cf unmap-route $old cfapps.io -n $curr
+cf unmap-route $old cfapps.io -n bosh
+cf unmap-route $old cloudfoundry.org -n bosh
 cf unmap-route $old bosh.io -n www
 cf unmap-route $old bosh.io
 
