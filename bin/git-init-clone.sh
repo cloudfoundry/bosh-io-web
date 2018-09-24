e51f42b7c71a9be83d90d8d85683475933447760
Joshua Aresty <joshua.aresty@emc.com>
2018-09-24 13:39:05 -0700

    Enable git-init-clone to clone into passed dir
    Co-authored-by: Danny Berger <dberger@pivotal.io>

--- bin/git-init-clone.sh
+++ bin/git-init-clone.sh
@@ -2,14 +2,16 @@
 
 set -e -x
 
+clone_dir="${clone_dir:-prod-conf}"
+
 echo "Clone data source"
-git clone https://github.com/bosh-io/releases                  prod-conf/releases
-git clone https://github.com/bosh-io/releases-index            prod-conf/releases-index
-git clone https://github.com/bosh-io/stemcells-legacy-index    prod-conf/stemcells-legacy-index
-git clone https://github.com/bosh-io/stemcells-core-index      prod-conf/stemcells-core-index
-git clone https://github.com/bosh-io/stemcells-cpi-index       prod-conf/stemcells-cpi-index
-git clone https://github.com/bosh-io/stemcells-windows-index   prod-conf/stemcells-windows-index
-git clone https://github.com/bosh-io/stemcells-softlayer-index prod-conf/stemcells-softlayer-index
+git clone https://github.com/bosh-io/releases                  $clone_dir/releases
+git clone https://github.com/bosh-io/releases-index            $clone_dir/releases-index
+git clone https://github.com/bosh-io/stemcells-legacy-index    $clone_dir/stemcells-legacy-index
+git clone https://github.com/bosh-io/stemcells-core-index      $clone_dir/stemcells-core-index
+git clone https://github.com/bosh-io/stemcells-cpi-index       $clone_dir/stemcells-cpi-index
+git clone https://github.com/bosh-io/stemcells-windows-index   $clone_dir/stemcells-windows-index
+git clone https://github.com/bosh-io/stemcells-softlayer-index $clone_dir/stemcells-softlayer-index
 
 (
   echo "In 1s"
