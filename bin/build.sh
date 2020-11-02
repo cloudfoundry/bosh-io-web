53ec04210934d39852828559a60253c8426df5c6
ramonskie <ramonmakkelie@gmail.com>
2020-11-02 10:16:38 +0100

    disable gomodules for now

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,7 @@ set -e
 echo "Configuring go"
 export GOARCH=amd64
 export GOOS=linux
+export GO111MODULE=off
 export GOTOOLDIR=$(go env GOROOT)/pkg/linux_amd64
 export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../../../.. && pwd )"
 
--- bin/run-local.sh
+++ bin/run-local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export GO111MODULE=off
+
 go build -o bosh-hub github.com/bosh-io/web/main
 
 exec ./run.sh conf/local.json -debug
