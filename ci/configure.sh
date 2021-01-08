9eed46d0f350a1ac90a0980a1d9c162382fe944d
Sam Coward <scoward@pivotal.io>
2021-01-08 11:06:24 -0500

    Use docker credentials from credhub

--- ci/configure.sh
+++ ci/configure.sh
@@ -6,5 +6,4 @@ fly --target be \
   set-pipeline \
   --pipeline bosh-io-web \
   --config ci/pipeline.yml \
-  --load-vars-from <( lpass show --fixed-strings --notes "bosh-io/web ci/pipeline.yml secrets (VMware IT PCF - formerly known as PCF Photon)" ) \
-  --load-vars-from <( lpass show --fixed-strings --notes "Shared-BOSH CPI (pivots only)/bosh-lite-concourse-secrets" )
+  --load-vars-from <( lpass show --fixed-strings --notes "bosh-io/web ci/pipeline.yml secrets (VMware IT PCF - formerly known as PCF Photon)" )
