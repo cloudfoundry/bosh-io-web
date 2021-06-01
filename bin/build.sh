5eb1bc8661499571fb8620806e65862d89dc303e
rkoster <hi@rkoster.dev>
2021-06-01 13:42:05 +0200

    Use latest go lang in ci

--- bin/build.sh
+++ bin/build.sh
@@ -5,9 +5,6 @@ set -e
 echo "Configuring go"
 export GOARCH=amd64
 export GOOS=linux
-export GO111MODULE=off
-export GOTOOLDIR=$(go env GOROOT)/pkg/linux_amd64
-export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../../../.. && pwd )"
 
 echo "Building bosh-hub"
 go build -o bosh-hub github.com/bosh-io/web/main
